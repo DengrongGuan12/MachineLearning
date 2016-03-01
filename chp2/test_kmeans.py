f7171ed22c7d65412548f15539c751ea243bf3ff
DengrongGuan12 <1031212913@qq.com>
2016-03-01 16:40:51 +0800

    kmeans

---
+++ chp2/test_kmeans.py
@@ -0,0 +1,23 @@
+__author__ = 'I322233'
+from numpy import *
+import time
+import matplotlib.pyplot as plt
+import kMeans as km
+
+## step 1: load data
+print "step 1: load data..."
+dataSet = []
+fileIn = open('kmeans_test_set.txt')
+for line in fileIn.readlines():
+    lineArr = line.strip().split(' ')
+    dataSet.append([float(lineArr[0]), float(lineArr[1])])
+
+## step 2: clustering...
+print "step 2: clustering..."
+dataSet = mat(dataSet)
+k = 4
+centroids, clusterAssment = km.kmeans(dataSet, k)
+
+## step 3: show the result
+print "step 3: show the result..."
+km.showCluster(dataSet, k, centroids, clusterAssment)
